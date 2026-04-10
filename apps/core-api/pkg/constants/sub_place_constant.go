78038eea47752c4a45a36f79dcc0021fc564846a
Codex2142 <sadatakbar04@gmail.com>
2026-04-10 16:18:41 +0700

    [ADD] Add Constant value

---
+++ apps/core-api/pkg/constants/sub_place_constant.go
@@ -0,0 +1,9 @@
+package constants
+
+type SubPlace string
+
+const (
+	Pond       SubPlace = "pond"
+	Hydroponic SubPlace = "hydroponic"
+	PlantBased SubPlace = "plant_based"
+)
---
+++ apps/core-api/pkg/constants/user_constant.go
@@ -0,0 +1,12 @@
+package constants
+
+type UserRole string
+
+const (
+	Admin   UserRole = "admin"
+	Partner UserRole = "partner"
+)
+
+// cara penggunaaan
+// import "farming/pkg/constants"
+// constants.Admin
